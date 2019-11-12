b777983d77cfd76fcb2b414d0bf56c93beacfad8
Long Yonggang <yonggang.long@nokia-sbell.com>
2019-11-12 14:46:38 +0800

    udpate eureka

--- account-service/src/main/java/com/longyg/account/services/AccountService.java
+++ account-service/src/main/java/com/longyg/account/services/AccountService.java
@@ -8,11 +8,14 @@ import com.longyg.account.model.Account;
 import com.longyg.account.model.AccountInfo;
 import com.longyg.account.model.Score;
 import com.longyg.account.repository.AccountRepository;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
 import java.util.UUID;
 
 @Service
@@ -37,7 +40,15 @@ public class AccountService {
         return accountRepository.findAll();
     }
 
+    @HystrixCommand(
+        fallbackMethod = "buildFallbackAccount",
+        commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000")
+        }
+    )
     public Account getAccount(String accountId) {
+        randomlyRunLong();
+
         Account account = accountRepository.findByAccountId(accountId);
         return account;
     }
@@ -73,6 +84,7 @@ public class AccountService {
         return ai;
     }
 
+    @HystrixCommand
     private Score getScore(String accountId, String clientType) {
         Score score = null;
         switch (clientType) {
@@ -92,5 +104,30 @@ public class AccountService {
                 break;
         }
         return score;
+    }
+
+    private void randomlyRunLong() {
+        Random rand = new Random();
+        int randomNum = rand.nextInt((3 - 1) + 1) + 1;
+        if (randomNum == 3) {
+            sleep();
+        }
+    }
+
+    private void sleep() {
+        try {
+            Thread.sleep(11000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Account buildFallbackAccount(String accountId) {
+        Account account = new Account();
+        account.setAccountId("-1");
+        account.setAccountName("dummy");
+        account.setAccountType("dummy");
+        account.setPassword("dummy");
+        return account;
     }
 }
