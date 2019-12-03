1b15a8610d8352498b0545ae795822c353b62132
Long <yonggang.long@nokia-sbell.com>
2019-12-03 17:26:03 +0800

    auth service

---
+++ auth-service/src/main/java/com/longyg/auth/security/OAuth2Config.java
@@ -0,0 +1,38 @@
+package com.longyg.auth.security;
+
+import com.longyg.auth.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+
+@Configuration
+public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("msclient")
+                .secret(passwordEncoder.encode("msclient"))
+                .authorizedGrantTypes("refresh_token", "password", "client_credentials", "authorization_code")
+                .redirectUris("http://www.baidu.com")
+                .scopes("all");
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager)
+                .userDetailsService(userService);
+    }
+}
