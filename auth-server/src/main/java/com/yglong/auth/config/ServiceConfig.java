924ff53299198d3ef97a7e00f4cbf750c8d8f92f
Long <yonggang.long@nokia-sbell.com>
2019-12-17 17:24:06 +0800

    init

---
+++ auth-server/src/main/java/com/yglong/auth/config/ServiceConfig.java
@@ -0,0 +1,18 @@
+package com.yglong.auth.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ServiceConfig {
+    @Value("${signing.key}")
+    private String jwtSigningKey;
+
+    public String getJwtSigningKey() {
+        return jwtSigningKey;
+    }
+
+    public void setJwtSigningKey(String jwtSigningKey) {
+        this.jwtSigningKey = jwtSigningKey;
+    }
+}
